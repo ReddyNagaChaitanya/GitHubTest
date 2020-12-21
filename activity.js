03f4f8ccc79887d9a65475de197890bfccd8e3f8
ReddyNagaChaitanya <76435378+ReddyNagaChaitanya@users.noreply.github.com>
2020-12-21 18:11:28 +0530

    Create activity.js

---
+++ activity.js
@@ -0,0 +1,20 @@
+var connection = new Postmonger.Session();
+
+ 
+
+//Startup Sequence
+connection.trigger('ready');
+
+ 
+
+connection.on('initActivity', function( data ) {
+document.getElementById( 'configuration' ).value = JSON.stringify( data, null, 2);
+});
+
+ 
+
+//Save Sequence
+connection.on('clickedNext', function() {
+var configuration = JSON.parse( document.getElementById( 'configuration' ).value);
+configuration.trigger('updateActivity', configuration);
+});
